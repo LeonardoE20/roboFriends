dd76c273cd0a04ea7088163c4e6a75b2ad08c1ba
Leonardo Escudero <leonardolearns@gmail.com>
2020-07-19 23:44:18 -0700

    created my first component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends React.Component {
+class App extends Component {
   render() {
     return (
       <div className="App">
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
+import Hello from './Hello';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <h1>Hello World!</h1>
+    <Hello />
   </React.StrictMode>,
   document.getElementById('root')
 );
