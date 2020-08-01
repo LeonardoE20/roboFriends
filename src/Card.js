93f3fee53e444456e6c0eb3261dfdc3d30a5c201
Leonardo Escudero <leonardolearns@gmail.com>
2020-07-31 21:11:11 -0700

    changed the size of the robot

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Card = () => {
+  return (
+    <div>
+      <img alt='robots' src='https://robohash.org//test?200x200' />
+      <div>
+        <h2>Jane Doe</h2>
+        <p>Jane.doe@gmail.com</p>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
