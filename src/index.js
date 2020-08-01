bbdf06a1a3b16659773359ce76ca0e9a90f87fc5
Leonardo Escudero <leonardolearns@gmail.com>
2020-08-01 01:35:57 -0700

    added arrays and called them for the information

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import './index.css';
 import Card from './Card';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
+import { robots } from './robots';
 
 ReactDOM.render(
   <React.StrictMode>
     <div>
-      <Card />
+      <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
       <Card />
       <Card />
     </div>
