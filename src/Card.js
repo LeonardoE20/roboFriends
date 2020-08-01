96cb3304265a28b33a32b2ce67d489e79b6eccf7
Leonardo Escudero <leonardolearns@gmail.com>
2020-08-01 16:33:23 -0700

    added fragment instead of a div tag to make it semantic (makes things much neater and easier to read)

--- src/Card.js
+++ src/Card.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import { Fragment } from 'react';
 
 const Card = ({ name, email, id }) => {
   return (
-    <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+    <Fragment className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
       <img alt='robots' src={`https://robohash.org//${id}?200x200`} />
-      <div>
+      <Fragment>
         <h2>{name}</h2>
         <p>{email}</p>
-      </div>
-    </div>
+      </Fragment>
+    </Fragment>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -5,14 +5,15 @@ import Card from './Card';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 import { robots } from './robots';
+import { Fragment } from 'react';
 
 ReactDOM.render(
   <React.StrictMode>
-    <div>
+    <Fragment>
       <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
       <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
       <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />
-    </div>
+    </Fragment>
   </React.StrictMode>,
   document.getElementById('root')
 );
