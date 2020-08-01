fe87caa45f8e8d9d39f161c73165ba9f0fdb67a7
Leonardo Escudero <leonardolearns@gmail.com>
2020-08-01 16:19:32 -0700

    I made my code even cleaner and easier by destructuring within the parameters

--- src/Card.js
+++ src/Card.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
-const Card = (props) => {
-  const { name, email, id } = props;
+const Card = ({ name, email, id }) => {
   return (
     <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
       <img alt='robots' src={`https://robohash.org//${id}?200x200`} />
