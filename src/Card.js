90a61a31bd810ddd568202bb70af21573d6e707e
Leonardo Escudero <leonardolearns@gmail.com>
2020-08-01 16:10:18 -0700

    used a template string to randomize what robots we get by calling the id

--- src/Card.js
+++ src/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = (props) => {
   return (
     <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-      <img alt='robots' src='https://robohash.org//test?200x200' />
+      <img alt='robots' src={`https://robohash.org//${props.id}?200x200`} />
       <div>
         <h2>{props.name}</h2>
         <p>{props.email}</p>
