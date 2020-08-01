89f5641b5cb6323b1ecb1b14c8968b45305b9bb7
Leonardo Escudero <leonardolearns@gmail.com>
2020-07-31 21:27:39 -0700

    added a shadow layer so that it looks more 3d to all of the ccards

--- src/Card.js
+++ src/Card.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Card = () => {
   return (
-    <div className='bg-light-green dib br3 pa3 ma2 grow'>
+    <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
       <img alt='robots' src='https://robohash.org//test?200x200' />
       <div>
         <h2>Jane Doe</h2>
