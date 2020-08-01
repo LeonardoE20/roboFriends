219b9bba6e08e53e454075f56400c5203d1423dc
Leonardo Escudero <leonardolearns@gmail.com>
2020-07-31 20:56:24 -0700

    added a new component and a new file

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Hello from './Hello';
+import Card from './Card';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Hello greeting={'Hello' + 'React Ninja'} />
+    <Card />
   </React.StrictMode>,
   document.getElementById('root')
 );
