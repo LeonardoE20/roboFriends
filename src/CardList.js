71c56f484bf663a4aca82a68ef277a9b40884ba6
Leonardo Escudero <leonardolearns@gmail.com>
2020-08-01 17:30:45 -0700

    CardList will be the one to return the cards and index.html will just render CardList

--- src/CardList.js
+++ src/CardList.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import { Fragment } from 'react';
 
 const CardList = () => {
   return (
-    
+    <Fragment>
+      <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
+      <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
+      <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />
+    </Fragment>
 
   );
 }
--- src/index.js
+++ src/index.js
@@ -10,11 +10,7 @@ import { Fragment } from 'react';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Fragment>
-      <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
-      <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
-      <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />
-    </Fragment>
+    <CardList />
   </React.StrictMode>,
   document.getElementById('root')
 );
