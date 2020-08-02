7cfd5eeca937106498fc16cb25192556d5547865
Leonardo Escudero <leonardolearns@gmail.com>
2020-08-01 18:20:10 -0700

    added a map loop in order to call of the arrays from the robots.js

--- src/Card.js
+++ src/Card.js
@@ -3,13 +3,13 @@ import { Fragment } from 'react';
 
 const Card = ({ name, email, id }) => {
   return (
-    <Fragment className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+    <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
       <img alt='robots' src={`https://robohash.org//${id}?200x200`} />
       <Fragment>
         <h2>{name}</h2>
         <p>{email}</p>
       </Fragment>
-    </Fragment>
+    </div>
   );
 }
 
--- src/CardList.js
+++ src/CardList.js
@@ -3,13 +3,13 @@ import Card from './Card';
 import { Fragment } from 'react';
 
 const CardList = ({ robots }) => {
+  const cardsArray = robots.map((user, i) => {
+    return <Card id={robots[i].id} name={robots[i].name} email={robots[i].email} />
+  })
   return (
     <Fragment>
-      <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
-      <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
-      <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />
+      {cardsArray}
     </Fragment>
-
   );
 }
 
