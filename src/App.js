7b9933a5aec6397a2db505d540ee0d95b835e24e
Leonardo Escudero <leonardolearns@gmail.com>
2020-08-02 13:51:19 -0700

    added a new file so that the search bar scrolls with the screen.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
-import { robots } from './robots';
-import SearchBox from './SearchBox'
+import SearchBox from './SearchBox';
+import Scroll from './Scroll'
 import './App.css';
 
 class App extends Component {
@@ -34,7 +34,9 @@ class App extends Component {
         <div className='tc'>
           <h1 className='f1'>RoboFriends</h1>
           <SearchBox searchChange={this.onSearchChange} />
-          <CardList robots={filteredRobots} />
+          <Scroll>
+            <CardList robots={filteredRobots} />
+          </Scroll>
         </div>
       );
     }
