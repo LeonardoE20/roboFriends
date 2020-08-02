b4c7774339eb77a6a0b5bd40f1122cacabcb8ef7
Leonardo Escudero <leonardolearns@gmail.com>
2020-08-01 18:34:11 -0700

    made my code even more clean and easy to read

--- src/CardList.js
+++ src/CardList.js
@@ -3,19 +3,20 @@ import Card from './Card';
 import { Fragment } from 'react';
 
 const CardList = ({ robots }) => {
-  const cardsArray = robots.map((user, i) => {
-    return (
-      <Card
-        key={i}
-        id={robots[i].id}
-        name={robots[i].name}
-        email={robots[i].email}
-      />
-    );
-  })
   return (
     <Fragment>
-      {cardsArray}
+      {
+        robots.map((user, i) => {
+          return (
+            <Card
+              key={i}
+              id={robots[i].id}
+              name={robots[i].name}
+              email={robots[i].email}
+            />
+          );
+        })
+      }
     </Fragment>
   );
 }
