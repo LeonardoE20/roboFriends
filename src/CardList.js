82bd95749e1abc50899a2fdd156c51859c1695a3
Leonardo Escudero <leonardolearns@gmail.com>
2020-08-01 18:24:27 -0700

    gave the loop a key so that if one of them loses information it will take that card out automatically

--- src/CardList.js
+++ src/CardList.js
@@ -4,7 +4,7 @@ import { Fragment } from 'react';
 
 const CardList = ({ robots }) => {
   const cardsArray = robots.map((user, i) => {
-    return <Card id={robots[i].id} name={robots[i].name} email={robots[i].email} />
+    return <Card key={i} id={robots[i].id} name={robots[i].name} email={robots[i].email} />
   })
   return (
     <Fragment>
