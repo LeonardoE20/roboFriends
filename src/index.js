e9eaf6ad29d834966fae706ceb45db59d7ba1fd6
Leonardo Escudero <leonardolearns@gmail.com>
2020-08-01 17:36:02 -0700

    took out the import card component from index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Card from './Card';
 import CardList from './CardList';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
