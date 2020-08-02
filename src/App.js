353ce2740820ac6cf98bc8833d9af42f726bfef0
Leonardo Escudero <leonardolearns@gmail.com>
2020-08-02 11:54:39 -0700

    added styles to the title again to make it bigger and adding a color of the font

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ class App extends Component {
     })
     return (
       <div className='tc'>
-        <h1>RoboFriends</h1>
+        <h1 className='f1'>RoboFriends</h1>
         <SearchBox searchChange={this.onSearchChange} />
         <CardList robots={filteredRobots} />
       </div>
