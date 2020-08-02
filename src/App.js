a199f23ed8c53acdafc4703b7d01c2a8cf302955
Leonardo Escudero <leonardolearns@gmail.com>
2020-08-01 22:51:28 -0700

    added a state that is able to change and effect an app and had to use a class in order for the state to work

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React { Component } from 'react';
 import CardList from './CardList';
 import { robots } from './robots';
 import SearchBox from './SearchBox'
 import { Fragment } from 'react';
 
-const App = () => {
-  return (
-    <div className='tc'>
-      <h1>RoboFriends</h1>
-      <SearchBox />
-      <CardList robots={robots} />
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      robots: robots,
+      searchfield: '',
+    }
+  }
+  render() {
+    return (
+      <div className='tc'>
+        <h1>RoboFriends</h1>
+        <SearchBox />
+        <CardList robots={robots} />
+      </div>
+    );
+  }
 }
 export default App;
