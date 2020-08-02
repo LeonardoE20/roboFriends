1438401777149a18dc87cec30bc1525a34fac3d2
Leonardo Escudero <leonardolearns@gmail.com>
2020-08-01 22:24:43 -0700

    created a new component to create a search bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import CardList from './CardList';
 import { robots } from './robots';
+import SearchBox from './SearchBox'
 import { Fragment } from 'react';
 
 const App = () => {
   return (
     <Fragment>
       <h1>RoboFriends</h1>
+      <SearchBox />
       <CardList robots={robots} />
     </Fragment>
   );
---
+++ src/SearchBox.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Fragment } from 'react';
+
+const SearchBox = () => {
+  return (
+    <Fragment>
+      <input type='search' placeholder='search robots' />
+    </Fragment>
+  );
+}
+
+export default SearchBox
